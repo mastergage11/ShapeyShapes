eef3e6d281ec2d226617417ac35e54e5205db9a2
mastergage11 <master.gage11@yahoo.com>
2020-02-28 12:41:00 -0500

    Initial commit

---
+++ src/Food.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Food extends Sprite {
+
+    public Food(Color color, int x, int y, int width, int height, Board board){
+
+        super(color, x, y, width, height, board);
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(getColor());
+        g.fillOval(getX(), getY(), getWidth(), getHeight());
+    }
+}
